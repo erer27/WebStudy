4145f87d5228f262c5e7bc0be758ab6a60c3f0d3
erer27 <oksdfn@gmail.com>
2025-01-24 17:48:18 +0900

    2025-01-24

---
+++ HTMLBoardProject/src/main/java/com/sist/board/MainClass.java
@@ -0,0 +1,13 @@
+package com.sist.board;
+
+public class MainClass {
+	public static void main(String[] args) {
+		String name="홍길동";
+		System.out.println("<html>");
+		System.out.println("<body>");
+		System.out.println("<h1>"+name+"</h1>");
+		System.out.println("</body>");
+		System.out.println("</html>");
+		
+	}
+}
